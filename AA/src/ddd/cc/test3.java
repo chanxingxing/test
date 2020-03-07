efaf6d30d83dcad4af8106d50629af30b68522ba
chan <chan@LAPTOP-IHH5LS0Q>
2020-03-07 14:56:34 +0800

    commint

--- AA/src/ddd/cc/test3.java
+++ AA/src/ddd/cc/test3.java
@@ -4,5 +4,8 @@ public class test3 {
 
 	public static void main(String[] args) {
 		System.out.println("test3");
+		
+		
+		//11111111111111111111111
 	}
 }
---
+++ AA/src/ddd/cc/test4.java
@@ -0,0 +1,5 @@
+package ddd.cc;
+
+public class test4 {
+
+}
